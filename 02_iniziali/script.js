47f99af812e5e5172f284b8dbff502ee3bb86587
BrunoMaiolo <brunosj@hotmail.it>
2026-01-09 15:27:00 +0100

    funzione vuota

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,7 +4,9 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function getInitials(nameArray){
+    
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
