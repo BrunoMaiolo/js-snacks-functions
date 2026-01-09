33f342ff4bbce688e27723e84943a4127ec833e1
BrunoMaiolo <brunosj@hotmail.it>
2026-01-09 15:48:31 +0100

    funzione vuota

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,7 +4,9 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function filterByLetter(nameArray, letter){
+    
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
