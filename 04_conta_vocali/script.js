1bf0cbcb8242fb1e49a712cfed42a12975f15951
BrunoMaiolo <brunosj@hotmail.it>
2026-01-09 16:08:45 +0100

    stampo in console

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -19,6 +19,6 @@ function countVowels(word) {
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(countVowels(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
